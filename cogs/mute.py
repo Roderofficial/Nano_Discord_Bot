bcb2889e457663ce7ea40ebb7f48dc9e2fea68fa
Radosław Pochopień <radek.p.p@wp.pl>
2021-05-13 11:41:21 +0200

    Poprawki mute

--- cogs/mute.py
+++ cogs/mute.py
@@ -7,12 +7,50 @@ import asyncio
 from discord.utils import get
 from datetime import datetime, timedelta
 
+
 class mute(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
-    #temp mute insert to database
+    # create temp mute role and update in database
+    async def role_generate(self, gid):
+        # get guild
+        guild = self.bot.get_guild(int(gid))
+
+        # create role
+        role = await guild.create_role(name="Mute", color=0x868686)
+
+        # update permissions
+        for a in guild.channels:
+            if a.type.name == "text":
+                await a.set_permissions(role, send_messages=False)
+
+        update_settings(str(gid), "mute", "role_id", role.id)
+
+
+
+    #give role
+    async def give_role(self, member: discord.Member, guild: discord.Guild):
+        data = download_settings(guild.id,"mute")
+        try:
+            #check if role is generated
+            data['role_id']
+            role = get(guild.roles, id=data['role_id'])
+        except:
+            #if not generate a role and reurn for variable
+            await self.role_generate(guild.id)
+            data = download_settings(guild.id, "mute")
+            role = get(guild.roles, id=data['role_id'])
+            print('no role')
+        finally:
+            #give the role for member
+            give_role = role
+            await member.add_roles(give_role)
+            print('give role')
+
+
+    # temp mute insert to database
     async def database_add(self, aiotid, gid, member_id, expire_time):
         """
         :param aiotid: Async_io task id
@@ -59,16 +97,16 @@ class mute(commands.Cog):
         mycursor.close()
         mydb.close()
 
-    #time convert
-    def convert_time_to_seconds(self,time):
+    # time convert
+    def convert_time_to_seconds(self, time):
         time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
         try:
             return int(time[:-1]) * time_convert[time[-1]]
         except:
             return None
 
-    #async task for tempmute
-    async def temp_mute_task(self, ctx, member, time, *reason):
+    # async task for tempmute
+    async def temp_mute_task(self, ctx, member: discord.Member, time, reason):
         """
 
         :param ctx: Context
@@ -78,69 +116,83 @@ class mute(commands.Cog):
         :return: Nothing
         """
 
-
-        #reason chceck
-        if len(reason[0]) == 0:
-            reason = "Brak"
+        # check reason
+        if reason:
+            if len(reason[0]) == 0:
+                reason = "Brak"
+            else:
+                reason = " ".join(reason[:])
         else:
-            reason = " ".join(reason[:])
+            reason = "Brak"
 
-        #get guild and other data
+        # get guild and other data
         guild_id = str(ctx.guild.id)
-        data = download_settings(guild_id, "mute")
-        role = get(ctx.guild.roles, id=data['role_id'])
 
-        #time and converts
+        # time and converts
         converted_time = self.convert_time_to_seconds(time)
 
-        #check time
+        # check time
         if converted_time is None:
             await ctx.channel.send("Podano niepoprawny czas!")
             return None
 
-        #check if muted
-        if role in member.roles:
-            await ctx.channel.send('Ten użytkownk jest już wyciszony')
-            return None
 
-        #converts
+        # converts
         datetime_end = datetime.now() + timedelta(seconds=converted_time)
         czas_czytelny = datetime_end.strftime("%d/%m/%Y %H:%M:%S")
 
-        #asyncio task rename
+        # asyncio task rename
         task = asyncio.current_task()
         task.set_name(str(ctx.message.id))
         task_id = ctx.message.id
 
-        #add to database
-        await self.database_add(task_id, guild_id,member.id,datetime_end)
+        #check if is member muted
+        is_muted = 0
+        try:
+            data = download_settings(guild_id, "mute")
+            role = get(ctx.guild.roles, id=data['role_id'])
+            # check role
+            if role in member.roles:
+                await ctx.channel.send('Ten użytkownk jest już wyciszony')
+                is_muted = 1
+                return None
+        except:
+            None
 
-        #embed message for channel
-        embed = discord.Embed(description=f"Powód: {reason} \nCzas: {time} \nWygasa: {czas_czytelny} \nPrzez:{ctx.author}", color=0xfff904)
+        # add to database
+        await self.database_add(task_id, guild_id, member.id, datetime_end)
+
+        # embed message for channel
+        embed = discord.Embed(
+            description=f"Powód: {reason} \nCzas: {time} \nWygasa: {czas_czytelny} \nPrzez:{ctx.author}",
+            color=0xfff904)
         embed.set_author(name=f"Wyciszono użytkownika: {member}", icon_url=member.avatar_url)
         embed.set_footer(text=f"Mute id: {ctx.message.id}")
 
-        #embed message for DM
-        dm = discord.Embed(description=f"Powód: {reason} \nCzas: {time} \nWygasa: {czas_czytelny} \nPrzez:{ctx.author}", color = 0xfff904)
+        # embed message for DM
+        dm = discord.Embed(description=f"Powód: {reason} \nCzas: {time} \nWygasa: {czas_czytelny} \nPrzez:{ctx.author}",
+                           color=0xfff904)
         dm.set_author(name=f"Wyciszono cię na serwerze: {ctx.guild}",
-                         icon_url=ctx.guild.icon_url)
+                      icon_url=ctx.guild.icon_url)
         dm.set_footer(text=f"Mute id: {ctx.message.id}")
 
-        #add mute role
+        # add mute role
         await ctx.channel.send(embed=embed)
         await member.send(embed=dm)
-        await member.add_roles(role)
+        await self.give_role(member, ctx.guild)
 
-        #sleep
+        # sleep
         await asyncio.sleep(int(converted_time))
 
-        #remove role
+        # remove role
+        data = download_settings(guild_id, "mute")
+        role = get(ctx.guild.roles, id=data['role_id'])
         await member.remove_roles(role)
         await self.database_remove(task_id)
 
-    #aync recovery auto unmute
+    # aync recovery auto unmute
     async def recovery_unmute(self, task):
-        #data
+        # data
         guild = self.bot.get_guild(int(task[2]))
         guild_id = str(task[2])
         data = download_settings(guild_id, "mute")
@@ -170,14 +222,13 @@ class mute(commands.Cog):
             # sleep
             await asyncio.sleep(int(time_to_second))
 
+
             await member.remove_roles(role)
             await self.database_remove(name_task)
 
-
-
-    #system recovery after stop
+    # system recovery after stop
     async def recover_system(self):
-        #db connect
+        # db connect
         mydb = mysql.connector.connect(
             host=settings.db_adres,
             user=settings.db_login,
@@ -193,24 +244,23 @@ class mute(commands.Cog):
         for a in myresult:
             await self.recovery_unmute(a)
 
-
-
-
-
-
-
-    #tempmute command
+    # tempmute command
     @commands.command(name="tempmute")
     @commands.has_permissions(kick_members=True)
     async def tempmute(self, ctx, member: discord.Member, time, *reason):
-        await self.temp_mute_task(ctx, member,time,reason)
+        await self.temp_mute_task(ctx, member, time, reason)
 
+    # temp gen role
+    @commands.command(name="tempgen")
+    @commands.is_owner()
+    async def tempgen(self, ctx, member: discord.Member):
+        await self.give_role(member,ctx.guild)
 
-    #mute command
+    # mute command
     @commands.command(name="mute")
     @commands.has_permissions(kick_members=True)
     async def mute(self, ctx, member: discord.Member, *reason):
-        #check reason
+        # check reason
         if reason:
             if len(reason[0]) == 0:
                 reason = "Brak"
@@ -218,38 +268,44 @@ class mute(commands.Cog):
                 reason = " ".join(reason[:])
         else:
             reason = "Brak"
-        #get guild and other data
+
+        # get guild and other data
         guild_id = str(ctx.guild.id)
-        data = download_settings(guild_id, "mute")
-        role = get(ctx.guild.roles, id=data['role_id'])
+        is_muted = 0
+        try:
+            data = download_settings(guild_id, "mute")
+            role = get(ctx.guild.roles, id=data['role_id'])
+            # check role
+            if role in member.roles:
+                await ctx.channel.send('Ten użytkownk jest już wyciszony')
+                is_muted = 1
+                return None
+        except:
+            None
 
-        #check role
-        if role in member.roles:
-            await ctx.channel.send('Ten użytkownk jest już wyciszony')
-            return None
-
-        #embed message for channel
-        embed = discord.Embed(description=f"Powód: {reason} \nCzas: ∞ \nWygasa: Nigdy \nPrzez:{ctx.author}", color=0xfff904)
+        # embed message for channel
+        embed = discord.Embed(description=f"Powód: {reason} \nCzas: ∞ \nWygasa: Nigdy \nPrzez:{ctx.author}",
+                              color=0xfff904)
         embed.set_author(name=f"Wyciszono użytkownika: {member}", icon_url=member.avatar_url)
         embed.set_footer(text=f"Mute id: {ctx.message.id}")
 
-        #embed message for DM
-        dm = discord.Embed(description=f"Powód: {reason} \nCzas: ∞ \nWygasa: Nigdy \nPrzez:{ctx.author}", color = 0xfff904)
+        # embed message for DM
+        dm = discord.Embed(description=f"Powód: {reason} \nCzas: ∞ \nWygasa: Nigdy \nPrzez:{ctx.author}",
+                           color=0xfff904)
         dm.set_author(name=f"Wyciszono cię na serwerze: {ctx.guild}",
-                         icon_url=ctx.guild.icon_url)
+                      icon_url=ctx.guild.icon_url)
         dm.set_footer(text=f"Mute id: {ctx.message.id}")
 
-        #add mute role
+        # add mute role
         await ctx.channel.send(embed=embed)
         await member.send(embed=dm)
-        await member.add_roles(role)
+        await self.give_role(member, ctx.guild)
 
     @commands.command(name="unmute")
     @commands.has_permissions(kick_members=True)
     async def unmute(self, ctx, member: discord.Member):
         guild_id = str(ctx.guild.id)
-        data = download_settings(guild_id, "mute")
-        role = get(ctx.guild.roles, id=data['role_id'])
+
 
         # db connect
         mydb = mysql.connector.connect(
@@ -271,17 +327,25 @@ class mute(commands.Cog):
             task.cancel()
             await self.database_remove(task_id)
 
+        # sprawdzenie czy ma mute
+        is_muted = 0
+        try:
+            data = download_settings(guild_id, "mute")
+            role = get(ctx.guild.roles, id=data['role_id'])
+            if role in member.roles:
+                await member.remove_roles(role)
+                await ctx.channel.send('Pomyślnie usunięto wyciszenie użytkownika!')
+            else:
+                await ctx.channel.send('Ten użytkownik nie jest wyciszony!')
+        except:
+                await ctx.channel.send('Ten użytkownik nie jest wyciszony!')
 
-        #sprawdzenie czy ma mute
-        if role in member.roles:
-            await member.remove_roles(role)
-            await ctx.channel.send('Pomyślnie usunięto wyciszenie użytkownika!')
-        else:
-            await ctx.channel.send('Ten użytkownik nie jest wyciszony!')
 
     @commands.Cog.listener()
     async def on_ready(self):
         await self.recover_system()
-#setup
+
+
+# setup
 def setup(self):
     print("Moduł cogs.mute załadowany!")
--- testy.py
+++ testy.py
@@ -1,2 +1,17 @@
-from cogs.stats import *
-stats.get_and_update_record_online()
+from multicolorcaptcha import CaptchaGenerator
+
+# Captcha image size number (2 -> 640x360)
+CAPCTHA_SIZE_NUM = 2
+
+# Create Captcha Generator object of specified size
+generator = CaptchaGenerator(CAPCTHA_SIZE_NUM)
+
+# Generate a captcha image
+captcha = generator.gen_captcha_image(difficult_level=4)
+
+# Get information
+image = captcha["image"]
+characters = captcha["characters"]
+
+# Save the image to a file
+image.save("W:\discordtest/test.png", "png")
